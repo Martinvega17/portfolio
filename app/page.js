2fca8d667aed234bc745b025ad5dcd921c4d738e
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-12-10 05:27:13 -0600

    Refactor code structure and import statements

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import HeroSection from './componnents/HeroSection'
-
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className='container mx-auto px-12 py-4'>
-      <HeroSection />
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+
       </div>
     </main>
-  )
+  );
 }
