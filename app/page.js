96395a7fda7fc3bc8a0c56073eb9175ca0f0e5a4
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-12-10 04:20:11 -0600

    Refactor container class in Home component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import HeroSection from './componnents/HeroSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <div className='container mx-auto px-12 py-4'>
       <HeroSection />
       </div>
