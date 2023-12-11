54aa55f31b4986d1204d7f69136d705075eeae6b
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-12-11 02:32:52 -0600

    Add ProjectsSection component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import AboutSection from "./components/AboutSection";
 import HeroSection from "./components/HeroSection";
+import ProjectsSection from "./components/ProjectsSection";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectsSection />
 
 
       </div>
