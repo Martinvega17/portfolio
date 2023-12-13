c5d733bd671bd0caec85eaaafffa5ab891b1e197
Martin Salvador Vega Perez <martinvega170500@gmail.com>
2023-12-13 03:43:37 -0600

    add component animation

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import AboutSection from "./components/AboutSection";
 import HeroSection from "./components/HeroSection";
+import AchievementsSection from "./components/AchievementsSection"
 import ProjectsSection from "./components/ProjectsSection";
 import Navbar from "./components/Navbar";
 import EmailSection from "./components/EmailSection";
@@ -10,6 +11,7 @@ export default function Home() {
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AchievementsSection />
         <AboutSection />
         <ProjectsSection />
         <EmailSection />
